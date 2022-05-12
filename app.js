5557e28ad5b231041df1722942b6135142c74974
Gerhard <gerhard.putter@yahoo.com>
2022-05-12 09:01:06 +0200

    Add template string.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLeads = ["hello", "champ", "clown"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el");
@@ -6,14 +6,20 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads()
+    inputEl.value = ''
 })
 
-let listItems = " ";
 
 
-for (let i = 0; i < myLeads.length; i++) {
-
-    listItems += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let listItems = " ";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
